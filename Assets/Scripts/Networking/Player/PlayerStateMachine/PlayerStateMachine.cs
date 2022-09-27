651768e5552f4f2834f937b8ff1a4a0aca2213d1
Roman Calderon <romanc647@gmail.com>
2022-09-27 19:17:06 -0400

    Change psm rpc call attribute to TargetRpc

--- Assets/Scripts/Networking/Player/PlayerStateMachine/PlayerStateMachine.cs
+++ Assets/Scripts/Networking/Player/PlayerStateMachine/PlayerStateMachine.cs
@@ -10,9 +10,9 @@ namespace SilverDogGames.Networking.FSM
         [SerializeField] private BaseState initialState = null;
 
         #region State overrides
-        [ClientRpc] public void RPC_Idle() => ChangeState<ImpairedState>();
-        [ClientRpc] public void RPC_Moving() => ChangeState<MovingState>();
-        [ClientRpc] public void RPC_Impaired() => ChangeState<ImpairedState>();
+        [TargetRpc] public void RPC_Idle() => ChangeState<ImpairedState>();
+        [TargetRpc] public void RPC_Moving() => ChangeState<MovingState>();
+        [TargetRpc] public void RPC_Impaired() => ChangeState<ImpairedState>();
         #endregion
 
         protected override BaseState GetInitialState()
